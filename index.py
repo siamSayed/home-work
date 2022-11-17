2975995349210a640b6d7aa1ed2656ac0439aab6
siam sayed <fatemab592@gmail.com>
2022-11-17 16:39:07 +0600

    1.0.0

---
+++ index.py
@@ -0,0 +1,15 @@
+a=100
+b=20
+c=50
+def func() :
+    if (a > b):
+        if(a<c) :
+            print("largest one is : c")
+
+        if (a > c) :
+            print("largest one is : a")
+    else :
+        print("largest one is : b")
+
+
+func()
